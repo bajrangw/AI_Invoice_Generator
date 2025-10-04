ac8122a7d97851f76f947cce698c471344e24771
bajrangw <bajrangwavare@gmail.com>
2025-10-04 19:53:19 +0530

    #

--- frontend/invoice-generator/src/utils/apiPaths.js
+++ frontend/invoice-generator/src/utils/apiPaths.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000"
+    : "https://ai-invoice-generator-seven.vercel.app";
 
 export const API_PATHS = {
   AUTH: {
